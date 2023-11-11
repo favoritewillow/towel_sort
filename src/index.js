449d34bc83dd250623feae22f1e728284e92aeaf
Natalie <favoritewillow@gmail.com>
2023-11-11 11:27:43 +0200

    init: towel sort (Saturday 11.11.2023 11:27am)

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+// You should implement your task here.
+
+module.exports = function towelSort (matrix) {  
+  if (!matrix) {
+      return [];
+  }
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 === 1) {
+          for (let j = matrix[i].length - 1; j >= 0; j--) {
+              result.push(matrix[i][j]);
+          }
+      } else {
+          for (let j = 0; j < matrix[i].length; j++) {
+              result.push(matrix[i][j]);
+          }
+      }
+  }
+  return result;
+}
+
+
